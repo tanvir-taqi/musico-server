4dec85d4c1de1594bf4dc0e06f660b0eebb18034
jhangkar <taqit3852@gmail.com>
2022-11-18 21:54:20 +0600

    update request solved

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     if (err) {
         console.log(err.message);
-        
+
     }
     else {
         console.log("database connection established");
@@ -36,15 +36,15 @@ client.connect(err => {
 
 
 
-const verifyJwt = (req,res,next) =>{
+const verifyJwt = (req, res, next) => {
     const authHeader = req.headers.authorization
-    if(!authHeader){
-       return res.status(401).send({message:"unothorized user"})
+    if (!authHeader) {
+        return res.status(401).send({ message: "unothorized user" })
     }
     const token = authHeader.split(' ')[1]
-    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET , (err,decoded)=>{
-        if(err){
-          return  res.status(401).send({message:"unothorized user"})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "unothorized user" })
         }
         req.decoded = decoded
         next()
@@ -52,47 +52,47 @@ const verifyJwt = (req,res,next) =>{
 }
 
 
-const run = async () =>{
-    try{
+const run = async () => {
+    try {
         const serviceCollection = client.db('musico').collection('services')
         const reviewCollection = client.db('musico').collection('reviews')
 
 
         //jwt token post api 
-        app.post('/jwt',(req,res)=>{
+        app.post('/jwt', (req, res) => {
             const user = req.body
-            const token = jwt.sign(user, process.env.ACCES_TOKEN_SECRET )
-            res.send({token})
+            const token = jwt.sign(user, process.env.ACCES_TOKEN_SECRET, { expiresIn: '10h' })
+            res.send({ token })
 
         })
 
 
         // load services from database and limit 
-        app.get('/services', async (req , res)=>{
-            
-            const seeLimit =parseInt(req.query.service)
+        app.get('/services', async (req, res) => {
+
+            const seeLimit = parseInt(req.query.service)
             const query = {}
             const count = await serviceCollection.estimatedDocumentCount()
-            const cursor = serviceCollection.find(query) 
+            const cursor = serviceCollection.find(query)
             const services = await cursor.limit(seeLimit).toArray()
-            res.send({count,services})
+            res.send({ count, services })
 
         })
 
         //post a service
         app.post('/services', async (req, res) => {
             const service = req.body
-            
+
             const result = await serviceCollection.insertOne(service)
             res.send(result)
         })
 
 
         // load single service details 
-        app.get('/services/:id', async (req, res)=>{
+        app.get('/services/:id', async (req, res) => {
             const serviceId = req.params.id
-            
-            const query = {_id: ObjectId(serviceId)}
+
+            const query = { _id: ObjectId(serviceId) }
             const service = await serviceCollection.findOne(query)
             res.send(service)
         })
@@ -101,7 +101,7 @@ const run = async () =>{
         // post reviews in database
         app.post('/reviews', async (req, res) => {
             const review = req.body
-            
+
             const reviewId = await reviewCollection.insertOne(review)
             res.send(reviewId)
         })
@@ -110,42 +110,63 @@ const run = async () =>{
         // get review by id 
         app.get('/reviews/:id', async (req, res) => {
             const reviewId = req.params.id
-            const query = {service : reviewId}
-            const cursor = reviewCollection.find(query).sort({date: -1})
+            const query = { service: reviewId }
+            const cursor = reviewCollection.find(query).sort({ date: -1 })
             const review = await cursor.toArray()
             res.send(review)
         })
 
+
+
+
         // get my review by email 
 
-        app.get('/reviews', verifyJwt, async (req, res) => {
+        app.get('/reviews', async (req, res) => {
 
-            const decoded = req.decoded
+            // const decoded = req.decoded
+            // console.log(decoded);
+
+
+            // if(decoded.email !== req.query.email){
+            //     return  res.status(403).send({message:"unothorized user"})
+            // }
+
             let query = {}
 
-            if(decoded.email !== req.query.email){
-                return  res.status(403).send({message:"unothorized user"})
+            if (req.query.email) {
+                query = { email: req.query.email }
             }
-          
-            
-            if(req.query.email){
-                query = {email: req.query.email}
-            }
-          
+
             const cursor = reviewCollection.find(query)
             const myReviews = await cursor.toArray()
-            res.send(myReviews)      
+            res.send(myReviews)
 
+        })
+
+
+        //  update review by review id 
+        app.patch('/reviews/:id', async (req, res) => {
+            const reviewId = req.params.id
+            const review = req.body
+            
+            const query = { _id: ObjectId(reviewId) }
+            const updateDoc = {
+                $set: {
+                    message: review.message
+                },
+            };
+            const result =  await reviewCollection.updateOne(query , updateDoc)
+            res.send(result) 
         })
 
 
 
         // delete review by id 
 
-        app.delete('/reviews/:id',verifyJwt, async (req,res)=>{
-            const id = req.params.id 
-            
-            const query = {_id : ObjectId(id)}
+        app.delete('/reviews/:id',  async (req, res) => {
+            const id = req.params.id
+
+            const query = { _id: ObjectId(id) }
             const result = await reviewCollection.deleteOne(query)
             res.send(result)
 
@@ -154,17 +175,17 @@ const run = async () =>{
 
 
     }
-    catch(err){
+    catch (err) {
         console.log(err.message);
-    }finally{
+    } finally {
 
     }
 
 }
 
-run().catch(err =>console.log(err.message))
+run().catch(err => console.log(err.message))
 
 
-app.listen(port,()=>{
-console.log('server is listening on port');
+app.listen(port, () => {
+    console.log('server is listening on port');
 });
