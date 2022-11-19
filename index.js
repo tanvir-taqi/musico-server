e7a8ee43d51ccaeff14c84358f31c75570ca3bd9
jhangkar <taqit3852@gmail.com>
2022-11-19 22:35:47 +0600

    server okay and deployed

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 const port = process.env.PORT || 5000
 const app = express();
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 
-// musico
-// kCto8dYGGzCCuocD
+
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Welcome to musico server');
 })
 
 
